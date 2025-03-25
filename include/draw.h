ab9662a160ad5a08b24303f6b28c908c33685cbe
Jannis <157202945+JannisElef@users.noreply.github.com>
2025-03-25 14:51:46 +0100

    Update draw.h

--- include/draw.h
+++ include/draw.h
@@ -21,15 +21,10 @@ typedef struct {
     void *prev;
 } Style;
 
-typedef struct { // unused
-    Image *img;
-    Style *style;
-} Canvas;
-
 
 static Style defaultStyle = {0,
                     0, 0,
-                    0xFFFFFFFF, 0xFFFF0000,
+                    (int)0xFFFFFFFF, (int)0xFFFF0000,
                     1, 
                     NULL};
 
@@ -39,28 +34,28 @@ static int numStyles = 1;
 
 
 void pushStyle() {
-    Style *newStyle = (Style *)malloc(sizeof(Style));
-    if (newStyle == NULL) {
-        printFunctionError("couldn't allocate new memory", "pushStyle(no.: %d)", currentStyle->styleID +1);
-    }
-    *newStyle = (Style){currentStyle->styleID +1,
-                        0, 0,
-                        0xFFFFFFFF, 0xFFFF0000,
-                        1, 
-                        currentStyle};
-    currentStyle = newStyle;
-    numStyles++;
+    // Style *newStyle = (Style *)malloc(sizeof(Style));
+    // if (newStyle == NULL) {
+    //     printFunctionError("couldn't allocate new memory", "pushStyle(no.: %d)", currentStyle->styleID +1);
+    // }
+    // *newStyle = (Style){currentStyle->styleID +1,
+    //                     0, 0,
+    //                     0xFFFFFFFF, 0xFFFF0000,
+    //                     1, 
+    //                     currentStyle};
+    // currentStyle = newStyle;
+    // numStyles++;
 }
 
 void popStyle() {
-    if (numStyles > 1) {
-        Style *prev = currentStyle->prev;
-        free(currentStyle);
-        currentStyle = prev;
-        numStyles--;
-    } else {
-        printFunctionWarning("no further popStyle() possible", "popStyle()");
-    }
+    // if (numStyles > 1) {
+    //     Style *prev = currentStyle->prev;
+    //     free(currentStyle);
+    //     currentStyle = prev;
+    //     numStyles--;
+    // } else {
+    //     printFunctionWarning("no further popStyle() possible", "popStyle()");
+    // }
 }
 
 void translate(int x, int y) {
@@ -73,7 +68,7 @@ void strokeWeight(int px) {
 }
 
 void fill(int col) {
-    currentStyle->fillCol = col;
+    col = col;
 }
 
 void stroke(int col) {
@@ -86,34 +81,34 @@ void pixel(Image *img, int x, int y, int col) {
 }
 
 /* draw horizontal line */
-void hline(Image *img, int x1, int x2, int y) {
+void hline(Image *img, int x1, int x2, int y, int col) {
     int x1_ = x1;
     int x2_ = x2;
     if(x1_ > x2_) swap(&x1_, &x2_);
     for(int i = 0; i < x2_ -x1_; i++) {
-        pixel(img, x1_ +i, y, currentStyle->fillCol);
+        pixel(img, x1_ +i, y, col);
 
     }
 }
 
 /* draw vertical line */
-void vline(Image *img, int x, int y1, int y2) {
+void vline(Image *img, int x, int y1, int y2, int col) {
     int y1_ = y1;
     int y2_ = y2;
     if(y1_ > y2_) swap(&y1_, &y2_);
     for(int i = 0; i < y2_ -y1_; i++) {
-        pixel(img, x, y1_ +i, currentStyle->fillCol);
+        pixel(img, x, y1_ +i, col);
         
     }
 }
 /* draw universal line */
-void line(Image *img, int x1, int y1, int x2, int y2) {
+void line(Image *img, int x1, int y1, int x2, int y2, int col) {
     int dx = abs(x2 - x1), sx = x1 < x2 ? 1 : -1;
     int dy = -abs(y2 - y1), sy = y1 < y2 ? 1 : -1;
     int err = dx + dy, e2;
 
     while (1) {
-        pixel(img, x1, y1, currentStyle->fillCol);
+        pixel(img, x1, y1, col);
         
         if (x1 == x2 && y1 == y2) break;
         e2 = 2 * err;
@@ -144,51 +139,50 @@ void lineGradient(Image *img, int x1, int y1, int x2, int y2, int numSteps, int 
         int col = lerpColors(gradientCols, size, t);
         
         printf("(x%fy%f) (x%fy%f)\n", startX, startY, endX, endY);
-
-        fill(col);
-        line(img, startX, startY, endX, endY);
+        
+        line(img, startX, startY, endX, endY, col);
     }
     popStyle();
 }
 
 /* draw rectangle border */
-void rectBorder(Image *img, int x, int y, int w, int h) {
-    hline(img, x, x +w, y);
-    hline(img, x, x +w, y +h);
+void rectBorder(Image *img, int x, int y, int w, int h, int col) {
+    hline(img, x, x +w, y, col);
+    hline(img, x, x +w, y +h, col);
 
-    vline(img, x, y, y +h);
-    vline(img, x +w, y, y +h);
+    vline(img, x, y, y +h, col);
+    vline(img, x +w, y, y +h, col);
 }
 /* draw filled rectangle */
-void rect(Image *img, int x, int y, int w, int h) {
+void rect(Image *img, int x, int y, int w, int h, int col) {
     for(int i = 0; i < h; i++) {
-        hline(img, x, x +w, y +i);
+        hline(img, x, x +w, y +i, col);
     }
 }
 
 /* set background to color */
 void background(Image *img, int col) {
-    int tmpCol = currentStyle->fillCol;
-    currentStyle->fillCol = col;
-    rect(img, 0, 0, img->width, img->height);
-    currentStyle->fillCol = tmpCol;
+    int tmpCol = col;
+    col = col;
+    rect(img, 0, 0, img->width, img->height, col);
+    col = tmpCol;
 }
 
 /* draw cirlce border */
-void circleBorder(Image* img, int x, int y, int r) {
+void circleBorder(Image* img, int x, int y, int r, int col) {
     int xi = 0;
     int yi = r;
     int d = 3 - 2 * r;
 
     while (yi >= xi) {        
-        pixel(img, x + xi, y + yi, currentStyle->fillCol);
-        pixel(img, x - xi, y + yi, currentStyle->fillCol);
-        pixel(img, x + xi, y - yi, currentStyle->fillCol);
-        pixel(img, x - xi, y - yi, currentStyle->fillCol);
-        pixel(img, x + yi, y + xi, currentStyle->fillCol);
-        pixel(img, x - yi, y + xi, currentStyle->fillCol);
-        pixel(img, x + yi, y - xi, currentStyle->fillCol);
-        pixel(img, x - yi, y - xi, currentStyle->fillCol);
+        pixel(img, x + xi, y + yi, col);
+        pixel(img, x - xi, y + yi, col);
+        pixel(img, x + xi, y - yi, col);
+        pixel(img, x - xi, y - yi, col);
+        pixel(img, x + yi, y + xi, col);
+        pixel(img, x - yi, y + xi, col);
+        pixel(img, x + yi, y - xi, col);
+        pixel(img, x - yi, y - xi, col);
 
         if (d < 0) {
             d = d + 4 * xi + 6;
@@ -201,16 +195,16 @@ void circleBorder(Image* img, int x, int y, int r) {
 }
 
 /* draw filled cricle */
-void circle(Image* img, int x, int y, int r) {
+void circle(Image* img, int x, int y, int r, int col) {
     for (int yi = -r; yi <= r; yi++) {
         for (int xi = -r; xi <= r; xi++) {
             if (xi * xi + yi * yi <= r * r) {
-                pixel(img, x + xi, y + yi, currentStyle->fillCol);
+                pixel(img, x + xi, y + yi, col);
 
             }
         }
     }
-    circleBorder(img, x, y, r);
+    circleBorder(img, x, y, r, col);
 }
 
 #pragma GCC diagnostic pop
